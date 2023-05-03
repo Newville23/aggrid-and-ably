a3eac6f7dd99177a7729dc273ab6c88523c5a77c
Villa <david.villanueva.dev@gmail.com>
2023-05-02 22:18:59 -0500

    feat(ably-grid-09): enhances cursor pointer for online users

--- src/components/Grid/components/CustomCellRender/CustomCellRender.tsx
+++ src/components/Grid/components/CustomCellRender/CustomCellRender.tsx
@@ -18,17 +18,26 @@ const CustomCellRender = (props: ICellRendererParams) => {
   )
   const cellBasicClass = 'relative h-full w-full'
 
-  const cellClass = userPointerInCell ? `border-dashed border-2` : ''
+  const cellClass = userPointerInCell ? `border-solid border-2` : ''
 
+  const userNameForPointer = userPointerInCell?.data.name
   return (
     <div
       className={`${cellBasicClass} ${cellClass}`}
       style={{
         borderColor: userPointerInCell ? userPointerInCell.color : 'inherit',
-        left: '-18px',
-        paddingLeft: '18px',
+        paddingRight: '17px',
+        paddingLeft: '17px',
       }}
     >
+      {userNameForPointer && (
+        <div
+          className="absolute z-50 text-white -top-5 -right-0.5 leading-4 py-0.5 px-2.5 rounded-t"
+          style={{ backgroundColor: userPointerInCell.color }}
+        >
+          {userNameForPointer}
+        </div>
+      )}
       <span>{cellValue}</span>
     </div>
   )
